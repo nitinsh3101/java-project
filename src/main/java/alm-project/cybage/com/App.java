4992313903dae93f82367837f402c21ea10906f5
Nitin Sharma <nitinsh3101@gmail.com>
2023-03-14 18:53:42 +0530

    first commit

---
+++ src/main/java/alm-project/cybage/com/App.java
@@ -0,0 +1,13 @@
+package alm-project.cybage.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
